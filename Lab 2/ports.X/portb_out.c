29e8b08a858e87529eac2afff5203ba4655a80d5
HBFisher <fisherhb@lafayette.edu>
2020-08-29 13:50:44 -0400

    Initial

---
+++ Lab 2/ports.X/portb_out.c
@@ -0,0 +1,29 @@
+#include <xc.h>
+#include <inttypes.h>
+//#include "portb_out.h"
+void portb_out_init() {
+  ANSELB = 0b00000000000000;
+  TRISB = 0b00000000000000; // set all port pins as inputs
+
+  //CNPUB = 0x1F; // enable internal pullups on all pins
+}
+void portb_out_write(uint16_t val)
+{
+// 0000 0000 0000 0000
+// RB 0000 X000 0X00 0000
+
+/*
+    if(val < 0x40){
+        PORTB = val;
+    }
+    else if(val >= 0x40 && val < 0x0800){
+      PORTB = val << 1;
+    }
+    else if(val >= 0x0800){
+        PORTB = val << 2;
+    }
+    */
+
+    PORTB = (val & 0x003F) | (val & 0x07C0) << 1 | (val & 0xF800) << 2;
+    //PORTB = 0xFFFF;
+}
