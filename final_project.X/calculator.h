2d5d567ee49fccf6401d38e18bf4c8ca16617c99
HBFisher <fisherhb@lafayette.edu>
2020-11-28 15:34:37 -0500

    update files

--- final_project.X/calculator.h
+++
@@ -1,8 +0,0 @@
-#ifndef CALCULATOR_H
-#define CALCULATOR_H
-#include <inttypes.h>
-
-extern int64_t tickFct_CALCULATOR(uint8_t button);
-//extern int64_t tickFct_CALCULATOR(uint8_t button);
-
-#endif
